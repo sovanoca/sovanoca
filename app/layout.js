6159898a4eba156e2d7438b0235d949aae004481
darshilkantaria11 <darshilkantaria11@gmail.com>
2025-07-05 20:58:55 +0530

    google analytics added

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./nopage/component/navbar"
 import Footer from "./nopage/component/footer"
+import Script from 'next/script';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,6 +20,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-CBDFQD5190"></Script>
+      <Script id="google-analytic">
+        {
+          `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+    
+            gtag('config', 'G-CBDFQD5190');
+              `
+        }
+
+      </Script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
